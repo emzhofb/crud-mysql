c4c8fd2398ce046ecf686b75f2124efb049595bc
emzhofb <ikhdamuhammad@gmail.com>
2019-02-04 16:36:40 +0700

    crud express dan sequelize (create and update)

--- routes/siswas.js
+++ routes/siswas.js
@@ -12,6 +12,10 @@ router.get('/', function(req, res, next) {
     })
 });
 
+router.get('/create',(req, res) => {
+    res.render('siswa/create')
+})
+
 router.get('/delete/:id',(req, res) => {
     const siswaId = req.params.id
     models.Siswa.findOne({where:
@@ -20,6 +24,47 @@ router.get('/delete/:id',(req, res) => {
     }}).then(siswa => {
         return siswa.destroy()
     }).then(siswa => {
+        res.redirect('/siswas')
+    }).catch(err => {
+        console.log(err)
+        res.redirect('/siswas')
+    })
+})
+
+router.post('/create',(req, res) => {
+    const { nama, alamat, kelas } = req.body
+    models.Siswa.create({nama, alamat, kelas}).then(siswa => {
+        res.redirect('/siswas')
+    }).catch(err => {
+        console.log(err)
+        res.redirect('/siswas')
+    })
+})
+
+router.get('/edit/:id', (req, res) => {
+    const siswaId = req.params.id
+    models.Siswa.findOne({where: {
+        id: siswaId
+    }}).then(siswa => {
+        res.render('siswa/edit',{siswa: siswa})
+    }).catch(err => {
+        console.log(err)
+        res.redirect('/siswas')
+    })
+})
+
+router.post('/edit/:id', (req, res) => {
+    const siswaId = req.params.id
+    const { nama, alamat, kelas } = req.body
+    models.Siswa.findOne({where: {
+        id: siswaId
+    }}).then(siswa => {
+        return siswa.update({
+            nama,
+            alamat,
+            kelas
+        })
+    }).then(updateSiswa => {
         res.redirect('/siswas')
     }).catch(err => {
         console.log(err)
